b7697d4171eb51c1d2f4b6e4cac16868ea05e1ac
Sukhdip Rai <sukhdipsrai@gmail.com>
2020-12-29 13:33:19 -0500

    pair switch

---
+++ src/moving_object.js
@@ -0,0 +1,11 @@
+function MovingObject(){
+
+    const constructor = function(obj){
+        this.pos = obj.pos;
+        this.vel = obj.vel;
+        this.radius = obj.radius;
+        this.color = obj.color;
+    };
+
+
+};
